45f20dd15e82a017036f896a9a3fd31490caca06
sqweek <sqweek@gmail.com>
2016-03-02 18:37:26 +0800

    playback: don't loop with a single beat selected

--- playback.go
+++ playback.go
@@ -174,7 +174,7 @@ func playToggle() {
 	}
 	playState = PLAYING
 	rng := G.ww.SelectedTimeRange()
-	if rng.MinFrame() == rng.MaxFrame() {
+	if rng.MinFrame() >= rng.MaxFrame() {
 		play(FrameRange{G.ww.FrameAtCursor(), G.wav.ToFrame(G.wav.NSamples) - 1})
 	} else {
 		play(rng)
